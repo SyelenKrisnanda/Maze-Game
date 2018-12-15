f75c731e34bfd240fa59fee8b2741b10060783bb
asus <asus@MORGAN>
2018-12-15 16:38:26 +0700

    ganti Author

--- src/model/Player.java
+++ src/model/Player.java
@@ -4,18 +4,24 @@ import java.awt.Image;
 import java.net.URL;
 import javax.swing.ImageIcon;
 
-public class Player extends Sel{
-    public Player(int x, int y){
-        super(x,y);
-        
+/**
+ *
+ * @author Project_MazeGame_175314080_175314103
+ */
+public class Player extends Sel {
+
+    public Player(int x, int y) {
+        super(x, y);
+
         URL loc = this.getClass().getResource("pemain.png");
         ImageIcon iia = new ImageIcon(loc);
-        
+
         Image image = iia.getImage();
-        
+
         this.setImage(image);
     }
-    public void Gerak(int x, int y){
+
+    public void Gerak(int x, int y) {
         int nx = this.getPosisiX() + x;
         int ny = this.getPosisiY() + y;
         this.setPosisiX(nx);
