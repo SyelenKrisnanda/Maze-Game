0b6e0e1d51927e24dd1ffa55f51548529f9bdfb2
asus <asus@MORGAN>
2018-12-15 16:38:02 +0700

    ganti Author

--- src/model/Finish.java
+++ src/model/Finish.java
@@ -4,10 +4,15 @@ import java.awt.Image;
 import java.net.URL;
 import javax.swing.ImageIcon;
 
-public class Finish extends Sel{
-    public Finish(int x, int y){
-        super(x,y);
-        
+/**
+ *
+ * @author Project_MazeGame_175314080_175314103
+ */
+public class Finish extends Sel {
+
+    public Finish(int x, int y) {
+        super(x, y);
+
         URL loc = this.getClass().getResource("finish.png");
         ImageIcon g = new ImageIcon(loc);
         Image image = g.getImage();
